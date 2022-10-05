cee531c8bd3df707dfdcaaf41823e2b0eb08425d
jwlei <joleiros@gmail.com>
2022-10-05 23:27:23 +0200

    Working on carousel text

--- js/scripts.js
+++ js/scripts.js
@@ -137,6 +137,50 @@
             });
         }
     }
+
+    // -----------------------------------------------------------------------------------
+    const carouselText = ["UI/UX", "Frontend", "Backend", "Fullstack", "JAVA"];
+    $(document).ready(async function() {
+        carousel(carouselText, "#feature-text")
+    });
+
+    async function typeSentence(sentence, eleRef, delay = 100) {
+        const letters = sentence.split("");
+        let i = 0;
+        while(i < letters.length) {
+            await waitForMs(delay);
+            $(eleRef).append(letters[i]);
+            i++
+        }
+    }
+
+    async function deleteSentence(eleRef) {
+        const sentence = $(eleRef).html();
+        const letters = sentence.split("");
+        let i = 0;
+        while(letters.length > 0) {
+            await waitForMs(100);
+            letters.pop();
+            $(eleRef).html(letters.join(""));
+        }
+    }
+
+    async function carousel(carouselList, eleRef) {
+        let i = 0;
+        while(true) {
+            await typeSentence(carouselList[i].text, eleRef);
+            await waitForMs(1500);
+            await deleteSentence(eleRef);
+            await waitForMs(500);
+            i++
+            if(i >= carouselList.length) {i = 0;}
+        }
+    }
+
+    function waitForMs(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
 })(jQuery);
 
 
@@ -284,49 +328,8 @@ function scrollFunction() {
     }
 }
 
-const carouselText = ["UI/UX", "Frontend", "Backend", "Fullstack", "JAVA"];
 
 
 
-$(document).ready(async function() {
-    carousel(carouselText, "#feature-text")
-});
 
-async function typeSentence(sentence, eleRef, delay = 100) {
-    const letters = sentence.split("");
-    let i = 0;
-    while(i < letters.length) {
-        await waitForMs(delay);
-        $(eleRef).append(letters[i]);
-        i++
-    }
-    return;
-}
 
-async function deleteSentence(eleRef) {
-    const sentence = $(eleRef).html();
-    const letters = sentence.split("");
-    let i = 0;
-    while(letters.length > 0) {
-        await waitForMs(100);
-        letters.pop();
-        $(eleRef).html(letters.join(""));
-    }
-}
-
-async function carousel(carouselList, eleRef) {
-    var i = 0;
-    while(true) {
-        updateFontColor(eleRef, carouselList[i].color)
-        await typeSentence(carouselList[i].text, eleRef);
-        await waitForMs(1500);
-        await deleteSentence(eleRef);
-        await waitForMs(500);
-        i++
-        if(i >= carouselList.length) {i = 0;}
-    }
-}
-
-function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
